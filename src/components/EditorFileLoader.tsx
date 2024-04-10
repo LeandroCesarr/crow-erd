a22e7b1242c85d37994336f28065130160f3e676
Leandro C. Silva <leandro.cesar@novahaus.com.br>
2024-04-09 21:57:37 -0300

    fix: fixes file loader button loading state

--- src/components/EditorFileLoader.tsx
+++ src/components/EditorFileLoader.tsx
@@ -4,12 +4,15 @@ import React, { FC } from 'react';
 import { Button } from './ui/button';
 import { useEditorFile } from '@/hooks/useEditorFile';
 import { SymbolIcon } from '@radix-ui/react-icons';
+import classNames from 'classnames';
 
 export const EditorFileLoader: FC = (): JSX.Element => {
   const { onFileChange, isLoading } = useEditorFile();
 
   return (
-    <Button asChild className="cursor-pointer" disabled={isLoading}>
+    <Button asChild className={classNames("cursor-pointer", {
+      "cursor-wait": isLoading
+    })} disabled={isLoading}>
       <label className="relative" htmlFor="load-editor-file">
         <input
           type="file"
@@ -17,10 +20,13 @@ export const EditorFileLoader: FC = (): JSX.Element => {
           id="load-editor-file"
           className="w-0 h-0"
           onChange={onFileChange}
-          accept='.json'
+          accept=".json"
+          disabled={isLoading}
         />
 
-        {isLoading ? <SymbolIcon className="mr-2 h-4 w-4 animate-spin" /> : null}
+        {isLoading ? (
+          <SymbolIcon className="mr-2 h-4 w-4 animate-spin" />
+        ) : null}
 
         <span>Open file</span>
       </label>
